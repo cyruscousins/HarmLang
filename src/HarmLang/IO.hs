c63dcbed2b9324c8f83eb3af141844edd25007db
Louis Rassaby <louis.rassaby@gmail.com>
2014-11-27 23:13:53 -0500

    midi integration

--- src/HarmLang/IO.hs
+++ src/HarmLang/IO.hs
@@ -6,6 +6,10 @@ module HarmLang.IO where
 import HarmLang.Types
 import Codec.Midi
 
+tempo = 500000
+granularity = 24
+notelength = granularity * 2
+
 -- http://stackoverflow.com/questions/26155872/creation-of-midi-file-in-haskell
 
 class MIDIable a where
@@ -16,34 +20,36 @@ instance MIDIable ChordProgression where
 
 instance MIDIable NoteProgression where
     outputToMidi prog file = let
-        convertToEvents [] _ = [(0,  TrackEnd)]
-        convertToEvents ((Note (Pitch (PitchClass p) o) (Time n d)):rest) start = let
+        convertToEvents [] = [(0,  TrackEnd)]
+        convertToEvents ((Note (Pitch (PitchClass p) o) (Time n d)):rest) = let
             num = 24 + (12 * o) + p
-            end = start + (24 * n) `div` d
+            start = 0
+            end = ((notelength * 2 * n) `div` d)
             in
-            (start,  NoteOn 0 num 80):(end, NoteOn 0 num 0):(convertToEvents rest end)
+            (start,  NoteOn 0 num 80):(end, NoteOn 0 num 0):(convertToEvents rest)
         in 
-        writeMIDI (convertToEvents prog 0) file
+        writeMIDI (convertToEvents prog) file
 
 instance MIDIable TimedChordProgression where
     outputToMidi prog file = let
-        convertToEvents [] _ = [(0,  TrackEnd)]
-        convertToEvents ((TimedChord (Harmony (PitchClass p) intervals) (Time n d)):rest) start = let
+        convertToEvents [] = [(0,  TrackEnd)]
+        convertToEvents ((TimedChord (Harmony (PitchClass p) intervals) (Time n d)):rest) = let
             root = 72 + p
-            end = start + (24 * n) `div` d
+            start = 0
+            end = start + ((notelength * n) `div` d)
             others = map (\(Interval i) -> root + i) intervals
             noteson  = map (\note -> (start, NoteOn 0 note 80)) (root:others)
             notesoff = map (\note -> (end, NoteOn 0 note 0)) (root:others)
             in
-            noteson ++ notesoff ++ (convertToEvents rest end)
+            noteson ++ notesoff ++ (convertToEvents rest)
         in 
-        writeMIDI (convertToEvents prog 0) file
+        writeMIDI (convertToEvents prog) file
 
 
 writeMIDI :: Track Ticks -> FilePath -> IO ()
 writeMIDI track file = 
-    exportFile file myMidi
+    exportFile file midi
     where
-    myMidi = Midi { fileType = SingleTrack, 
-                timeDiv  = TicksPerBeat 24, 
-                tracks   = [track] }
+    midi = Midi { fileType = SingleTrack, 
+                timeDiv  = TicksPerBeat granularity, 
+                tracks   = [(0, TempoChange tempo):track]}
