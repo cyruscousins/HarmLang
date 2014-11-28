d6b20eb4b35b5d1a131166ecf107e6997b0cd332
Cyrus Cousins <originalcyruscousins@gmail.com>
2014-11-28 18:09:28 -0500

    Added ChordProgressionDatabase

--- src/HarmLang/ChordProgressionDatabase.hs
+++ src/HarmLang/ChordProgressionDatabase.hs
@@ -2,7 +2,10 @@
 module ChordProgressionDatabase
 where
 
+import Data.List
+import Data.Maybe
 import qualified Data.Map
+--import qualified Data.Maybe
 
 import HarmLang.Types
 
@@ -13,14 +16,22 @@ data ChordProgressionDatabase = ChordProgressionDatabase [((Data.Map.Map String 
 --splitByCriterion :: ChordProgressionDatabase -> String -> [(String, [ChordProgression])
 
 getProgressionsInCategoryByCriterion :: ChordProgressionDatabase -> String -> String -> [ChordProgression] 
-extractByCriterion (ChordProgressionDatabase list) criterion category = map snd (filter (\ (map, _) -> ((==) (Data.Map.lookup map criterion) (Just category))) list)
+getProgressionsInCategoryByCriterion (ChordProgressionDatabase list) criterion category = map snd (filter (\ (map, _) -> ((==) ((flip Data.Map.lookup) map criterion) (Just category))) list)
 
 getCategoriesInCriterion :: ChordProgressionDatabase -> String -> [String]
+--getCategoriesInCriterion (ChordProgressionDatabase list) criterion = Data.Map.mapMaybe ((flip Data.Map.lookup) criterion $ fst) list
+getCategoriesInCriterion (ChordProgressionDatabase list) criterion = Data.Maybe.mapMaybe (\ cpMap -> Data.Map.lookup criterion cpMap) (map fst list)
 
 getProgressionsCategorizedByCriterion :: ChordProgressionDatabase -> String -> [(String, [ChordProgression])]
+getProgressionsCategorizedByCriterion cpd criterion = map (\catName -> (catName, getProgressionsInCategoryByCriterion cpd criterion catName)) $ getCategoriesInCriterion cpd criterion
+
+compareBy :: (Ord b) => (a -> b) -> (a -> a -> Ordering)
+compareBy f = (\ a1 a2 -> compare (f a1) (f a2))
 
 sortGroupsByName :: [(String, [ChordProgression])] -> [(String, [ChordProgression])]
+sortGroupsByName = sortBy (compareBy fst)
 
 sortGroupsBySize :: [(String, [ChordProgression])] -> [(String, [ChordProgression])]
+sortGroupsBySize = sortBy (compareBy (length . snd))
 
-
+--loadChordProgressionDatabase :: String -> IO ChordProgressionDatabase
